5ed6df74bc35088d883101157554c08e661b8ef5
Shangyang356 <qinchuan@QinChuans-MacBook-Pro.local>
2016-08-25 23:28:36 -0400

    fix empty check bugs
    merge only works on 1st row/column

--- 2048.c
+++ 2048.c
@@ -4,7 +4,9 @@
 #include <time.h>
 #include <math.h>
 
-int basearray[16];
+
+int arraya[16];
+int* basearray[16];
 /*
 ---------------
 0 | 1 | 2 | 3
@@ -17,7 +19,7 @@ int basearray[16];
 ---------------
 */
 
-int upbased[16];
+int* upbased[16];
 /*
 ---------------
 0 | 4 | 8 | 12
@@ -34,10 +36,15 @@ int *empty[16];
 int emptynum;
 unsigned long score;
 //update the upbased array.
-int update(){
+int init(){
 	int i;
+
 	for(i = 0; i<16;i++){
-		*(upbased+i) = *(basearray +(i/4 + i%4*4));
+		arraya[i] = 0;
+	}
+	for(i = 0; i<16;i++){
+		basearray[i] =&arraya[i];
+		upbased[i] = &arraya[(i/4 + i%4*4)];
 	}
 	return 0;
 }
@@ -52,25 +59,27 @@ int exchange(int* a, int* b){
 
 //direction is 0 when press up and left, 1 when press down and right.
 //move one column/row that all the elements stick together.
-int move(int* array,int direction){
+
+
+int moverow(int** array,int direction){
 	int i,j;
 	if(direction == 0){
-		for(i = 0;i<4;i++){
-			if(array[i] == 0){
+		for(i = 0;i < 3; i++){
+			if(*array[i] == 0){
 				for(j=i+1;j<4;j++){
-					if(array[j] != 0){
-						exchange(array+i,array+j);
+					if(*array[j] !=0){
+						exchange(array[i],array[j]);
 					}
 				}
 			}
 		}
 	}
-	else{
-		for(i = 3; i>=0;i--){
-			if(array[i] == 0){
-				for(j = i-1;j>=0;j--){
-					if(array[j] != 0){
-						exchange(array+i,array+j);
+	else if(direction == 1){
+		for(i = 3;i >0; i--){
+			if(*array[i] == 0){
+				for(j=i-1;j>0;j++){
+					if(*array[j] !=0){
+						exchange(array[i],array[j]);
 					}
 				}
 			}
@@ -79,15 +88,26 @@ int move(int* array,int direction){
 	return 0;
 }
 
+
+
+int move(int** array,int direction){
+	moverow(array,direction);
+	moverow(array+4,direction);
+	moverow(array+8,direction);
+	moverow(array+12,direction);
+	return 0;	
+}
+
 //merge column/row from left or up.
-int mergeleft(int* array){
+int mergerowleft(int** array){
 	int i;
 	
 	move(array,0);
 	for(i=0;i<3;i++){
-		if(array[i] == array[i+1] && array[i] != 0){
-			array[i] *=2;
-			array[i+1] = 0;
+		if(*(array[i]) == *(array[i+1]) && *(array[i]) != 0){
+			*(array[i]) *=2;
+			score += *(array[i]);
+			*(array[i+1]) = 0;
 		}
 	}
 	move(array,0);
@@ -95,16 +115,24 @@ int mergeleft(int* array){
 	return 0;
 }
 
+int mergeleft(int** array){
+	mergerowleft(array);
+	mergerowleft(array+4);
+	mergerowleft(array+8);
+	mergerowleft(array+12);
+	return 0;
+}
+
 //merge column/row from right or bottom.
-int mergeright(int *array){
+int mergeright(int **array){
 	int i;
 	
 	move(array,1);
 	for(i=3;i>0;i--){
-		if(array[i] == array[i-1] && array[i] != 0){
-			array[i] *=2;
-			score += array[i];
-			array[i-1] = 0;
+		if(*(array[i]) == *(array[i-1]) && *(array[i]) != 0){
+			*(array[i]) *=2;
+			score += *(array[i]);
+			*(array[i-1]) = 0;
 		}
 	}
 	move(array,1);
@@ -114,68 +142,63 @@ int mergeright(int *array){
 
 
 
-/*update the number of empty slots and collect all of their pointers in one array.*/
+//update the number of empty slots and collect all of their pointers in one array.
 int checkempty(){
 	int i;
 	emptynum = 0;
+	int j = 0;
 	for(i = 0;i < 16; i++){
-		if(basearray[i] == 0){
+		if(*basearray[i] == 0){
 			emptynum ++;
-			empty[i] = basearray + i;
+			empty[j] = basearray[i];
+			j++;
 		}
 	}
+	
 	return 0;
 }
 
 int action(){
 	int c = getchar();
-	c =getchar();
-	c = getchar();
-	switch(c){
-		case 65:
-			printf("press up!\n");
-			break;
-		case 66:
-			printf("press down!\n");
-			break;
-		case 67:
-			printf("press right!\n");
-			break;
-		case 68:
-			printf("press left!\n");
-			break;
-		}
+	while(c == 27 || c == 10 || c == 91){
+		c = getchar();
+	}
 
-	return 0;
-}
-/*
-int action2(){
-	if (getch() == '\033') { // if the first value is esc
-    	getch(); // skip the [
-    	switch(getch()) { // the real value
-        	case 'A':
-        		printf("press up!\n");
-            // code for arrow up
-            	break;
-        	case 'B':
-        		printf("press up\\down!\n");
-            // code for arrow down
-            	break;
-        	case 'C':
-        		printf("press right!\n");
-            // code for arrow right
-            	break;
-        	case 'D':
-        		printf("press left!\n");
-            // code for arrow left
-            	break;
-    	}
+	if(c == 65){
+		
+		move(upbased,0);
+
+		mergerowleft(upbased);
+		mergerowleft(upbased+4);
+
+
+		printf("press up!\n");
+	}
+	else if(c == 66){
+		printf("press down!\n");
+	}
+	else if(c == 67){
+		printf("press right!\n");
+	}
+	else if(c == 68){
+		move(basearray,0);
+		mergeleft(basearray);
+		checkempty();
+		printf("press left!\n");
 	}
 	return 0;
 }
-*/
 
-/*generate a new number in one of empty slot*/
+int testpress(){
+	int c = getchar();
+	while(c == 27 || c == 10 || c == 91){
+		c = getchar();
+	}
+	printf("c is %d\n", c);
+	return 0;
+}
+
+//generate a new number in one of empty slot
 int generatenew(){
 	int newnum;
 	int r =rand()%2;
@@ -186,18 +209,27 @@ int generatenew(){
 		newnum = 4;
 	}
 	checkempty();
+	int i;
+	for(i = 0;i < 16; i++){
+		if(*basearray[i] != 0){
+			printf("%dth in basearray is not empty\n", i+1);
+		}
+	}
 	int r2 = rand() % emptynum;
-	int* ptr = empty[r2];
-	*ptr = r;
+	*(empty[r2]) = newnum;
+	printf("empty pos is %ldth and newnum is %d\n",(empty[r2]-basearray[0])+1 ,newnum);
+	emptynum--;
+	printf("emptynum is %d\n",emptynum );
+
 	return 0;
 }
 
-/*print the 4*4 matrix*/
+//print the 4*4 matrix//
 
-int print(){
+int printmatrix(){
 	int i;
 	for(i = 0; i < 16;i++){
-		printf("%d    \n",basearray[i] );
+		printf("%d    ",*(basearray[i]) );
 		if(i%4 == 3){
 			printf("\n");
 			printf("\n");
@@ -214,7 +246,7 @@ int checklose(){
 		int i,j;
 		for(i = 0;i<4; i++){
 			for(j=0;j<3;j++){
-				if(basearray[j] == basearray[j+1] || upbased[j] == upbased[j+1]){
+				if(*(basearray[j]) == *(basearray[j+1]) || *(upbased[j]) == *(upbased[j+1])){
 					return 1;
 				}
 			}
@@ -229,14 +261,41 @@ int printscore(){
 }
 
 
-int main() {
 
-   	
+int play(){
+	checkempty();
+	while(checklose() ==1){
+		action();
+		generatenew();
+		printmatrix();
+		
+	}
+	printscore();
+	return 0;
+}
+
+int testarray(){
+	int i;
+	for(i = 0;i<16;i++){
+		printf("upper %d is %d\n", i, *upbased[i]);
+		printf("base %d is %d\n", i,*basearray[i]);
+	}
+	return 0;
+}
+
+int main() {
+	
 	srand(time(NULL));
+	init();
+	//testarray();
 	generatenew();
 	generatenew();
 	score = 0;
-	printf("press a key!\n");
-	action();
+	printmatrix();
+	while(0){
+		testpress();
+	}
+	play();
+	//testarray();
    	return 0;
 }
